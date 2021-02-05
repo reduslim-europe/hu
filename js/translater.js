e3fe773839030a4692de06640b83c62eea22b87b
reduslim-europe <78624243+reduslim-europe@users.noreply.github.com>
2021-02-06 00:47:05 +0200

    1

---
+++ js/translater.js
@@ -0,0 +1,165 @@
+var new_lang =   {
+
+"lt0": "Biztonságos minden <br>korban",
+
+"lt1": "A hatékonyság bizonyított - </b>a nők </b>98%</b>-a fogyott",
+
+"lt2": "<br>2017</br>-ben <br>304.342 darabot értékesítettek",
+
+"lt3": "Kapjon konzultáció",
+
+"lt4": "A XX. Kapszula 2 héten belül feloldja <br>akár <span class=\"pink\">10 kg </span>zsírt",
+
+"lt5": "Mentse magát az elhízástól és csökkenti a szívroham, a cukorbetegség és a magas vérnyomás kockázatát 89% -kal",
+
+"lt6": "Hatékonyan felosztja a ZSÍR<br> az <br>XX alfa-gyümölcssavaknak köszönhetően",
+
+"lt7": "Csökkenti a cukorbetegség, <br>magas vérnyomás, infarktus kialakulásának kockázatát",
+
+"lt8": "Megakadájoza a <br>veszélyes az életre <br>belső szervek elhízást",
+
+"lt9": "*Elnyomja az étvágyat",
+
+"lt10": "AKCIÓ! <span class=\"pink\">Csak 1 nap</span>",
+
+"lt11": "Az akció végéig maradt:",
+
+"lt12": "Maradt<br>akciós<br>termék:",
+
+"lt13": "darab",
+
+"lt14": "Önnek<span class=\"red\">EZ</span> ismerős?",
+
+"lt15": "Légszomjat tapasztalom<br>lépcsőn  <br>járás közben ",
+
+"lt16": "Erősen izzadok, <br>különösen nyáron",
+
+"lt17": "A hangulat naponta többször <br>változik",
+
+"lt18": "Éjszaka rosszul alszom, <br>és napközben álmos vagyok",
+
+"lt19": "Húzó f<br>ájdalmat érzem az ízületekben, <br>derékben",
+
+"lt20": "Nap végén nagyon fáradt, duzzadt lábak",
+
+"lt21": "A nyomás <br>emelkedik <br>és a fejem fáj",
+
+"lt22": "Több mint <br>38 éves vagyok",
+
+"lt23": "Ha <span class=\"red\">IGEN-el </span>válaszoltál 1 vagy több kérdésre -<div class=\"text-bold\">az elhízás kockázata és annak szörnyű következményei <br>nagyon magasak!</div>",
+
+"lt24": "14 millió, <br><div class=\"title title2\">1, 2, 3 fokos elhízással <br>nő már beteg!</div>",
+
+"lt25": "Ha a súly <brmeghaladta a normát 10% -kal, a <div class=\"text text2\">stroke kockázata, a koszorúér-elégtelenség</div> 7-szeresére nő",
+
+"lt26": "Stroke <br>és magas vérnyomás",
+
+"lt27": "<div class=\"text text2\"A cukorbetegséget <br>csendes gyilkosnak nevezik.</div>  A betegség kialakulásának<br> kockázata a <br>túlsúlynál <br>40-szeresével nő ",
+
+"lt28": "Cukorbetegség",
+
+"lt29": "Belső <div class=\"class title5\">szervek elhízása</div>",
+
+"lt30": "A szív elhízása",
+
+"lt31": "A zsírsejtek bejutnak a szív <br>izomszövetébe. A fő testünk hirtelen megállhat.<br><div class=\"text text_bold\">Az első tünetek - bizsergés a mellkasban.</div>",
+
+"lt32": "A tüdő elhízása",
+
+"lt33": "Tüdő elhízása -  pulmonális hipertónia <br>jobb kamrai károsodáshoz és <br>korai halálhoz vezet.<br> <div class=\"text text_bold\">Az első tünetek - légszomj.</div>",
+
+"lt34": "A máj elhízása",
+
+"lt35": "Máj elhízása - a zsíros hepatosis <br>akut cirrhosishoz vezet.<br> <div class=\"text text_bold\">Az első tünetek - álmosság,  gyengeség.</div>",
+
+"lt36": "Indítsa el<span class=\"green\">a zsírégetést </span> most!",
+
+"lt37": "Gyomor káros<br> diéták nélkül",
+
+"lt38": "Szív gyilkoló <br>edzés nélkül",
+
+"lt39": "Gyötrelmes<br> éhség nélkül",
+
+"lt40": "Használj olyan eszközt, <br>amelynek hatékonyságában az egész nemzet meg volt győződve!",
+
+"lt41": "Hogyan <br><span class=\"green\">befolyásolja XX az egészségét</span>",
+
+"lt42": "<b>XX xantont tartalmaz - <br> a legerősebb antioxidánsok!</b><br><br>Mérgező anyagokat, salakokat, <br>méreganyagokat, apróra vágott zsírt nyernek, amelyeket <br>nem vonnak le önmagukból<br><br>Megakadályozza <br>a belső szervek elhízását.",
+
+"lt43": "Hogyan befolyásolja a <br><span class=\"pink\"XX az alakját</span>",
+
+"lt44": "<b>Gyümölcs alfa-savak<br>szívódik fel a vérbe, és perceken belül biztonságosan f<br>eloszlattja a zsírt!</b><br><br>A csípő szűkebbé válik, a gyomor <br>megfeszül, a narancsbőr és a <br>túlsúly elmegy. ",
+
+"lt45": "Az éhség eltűnik!",
+
+"lt46": "A kapszulák szintetizálják a glikogént, és <br> \"jele vagyok\" jelet küldenek az agynak.",
+
+"lt47": "A dietetikus véleménye",
+
+"lt48": "Sajnos hazánkban csak a szépség kedvéért fogynak, elfelejtve, hogy a túlsúly fenyegetést jelent az életre! A súlyproblémák okozta női halálozás 27% -kal nőtt az előző évtizedhez képest. Cukorbetegség, magas vérnyomás, stroke, szívrohamok, vérrögök, májhájsejtek <br><br>Ha úgy érzed, hogy a testsúlyod nem normális - azonnal tegyen lépéseket! Ne várjon szívmegállást!<div class=\"text text-bold\"><br>A fejlett országokban a XX gyógyszer már régóta az egyik legbiztonságosabb és legnépszerűbb gyógyszer. Mindenkinek ajánlom, aki javítani kívánja az egészségét <br>és fogyni. </div> ",
+
+"lt49": "Bátri Márta<div class=\"text\"><br>Dietetikus</div>",
+
+"lt50": "A nők ezrei visszanyertés <br> <span class=\"pink\">egészségüket és harmóniájukat</span>",
+
+"lt51":  " 36 vagyok, XX -el 2 hónap alatt <br>lefogytam. Senki sem hitte, hogy én vagyok!<br>Mondták hogy kicseréltek:)))) <div class=\"author\">Zsuzsa, Budapest</div>",
+
+"lt52": "41 vagyok. Barátom ajánlotta <br>kapszula kúrát. A leginkább <br>kellemes diéta, amelyen<br> ültem. 3 hónap alatt - 25 kg. Az álmom <br>valóra vált! <div class=\"author\">Kata, Beregszász</div>",
+
+"lt53": "52 vagyok,  megelőzésére<br> minden évben XX használom. <br>És most 3 évig a <br>súlyom stabil 60 kg. És valamikor<br>89 kg voltam.<div class=\"author\">Viktória, Vác</div>",
+
+"lt54": "Egész életemben diétáztam,<br> de semmi sem segített. Nagyon szerettem az <br>édeset és jól <br>enni. Amikor az orvos azt mondta, hogy a szívem és a nyomás <br>problémái <br>összefüggenek a zsírokkal, megértettem, hogy gyorsan <br>fogyni kell, de biztonságosan. Az én<br>XX-em lett a <br>megmentés! <div class=\"author\">Papp Márta, Győr</div>",
+
+"lt55": "A férjem és én a válás szélén álltunk. <br>Egészségügyi problémák miatt dolgoztam. Stresszben <br>ültem otthon, és ettem. Amikor a XX <br>megjelent az életemben, a mozgás öröme és a <br>férjem szerelme <br>visszatért hozzám. Boldog vagyok! <div class=\"author\">Varga Katalin, Székesfehérvár</div>",
+
+"lt56": "Mindent ettem, ami a karom alá <br>került. Ha rosszat akarok <br>enni, én választom az XX.<br>Után süteményeket és <br>sütiket nem akaródzik enni. Számomra eddig is hihetetlen, hogy nem szenvedtem az edzőteremben, nem szenvedett diétával, és annyira foygtam!! <div class=\"author\">Szücs  Ilona Pécs</div>",
+
+"lt57": "Többi vélemények az XX-ről",
+
+"lt58": "Minimalizálja a véleményeket",
+
+"lt59": "Alkalmazás módja",
+
+"lt60": "Naponta háromszor <br>fehér kapszula reggel <br> vörös-barna délután és egy világoskék este",
+
+"lt61": "Óvakodjon a hamisítványoktól!",
+
+"lt62": "A XX <br>népszerűség<br> növekedésével",
+
+"lt63": "<br>gyakoriak a<br> hamisítások",
+
+"lt64": "<br>hulladékárakkal.",
+
+"lt65": "Csak itt <br>tudsz vásárolni <br>eredeti terméket",
+
+"lt66": "különleges áron",
+
+"lt67": "- Nem mutatkozott be",
+
+"lt68": "Hogyan működünk",
+
+"lt69": "Egy <br>azonnali megrendelést <br>rendelhet egy egyedi <br>áron <span class=\"price_main\">1 forintért</span>!",
+
+"lt70": "Várja meg az <br>operátor visszahívását a részletek <br>tisztázására",
+
+"lt71": "Kockázat nélkül!<br>Vedd át a csomagot, <br>és fizess <br>postán",
+
+"lt72": "Megrendeléseket fogadunk el egész <br><span class=\"pink\">Európában</span>",
+
+"lt83": "* Akciós ár csak<script type=\"text/javascript\">d = new Date(); p = new Date(d.getTime() -0*86400000);monthA = 'Január, Február, Március, Április, Május, Június, Július, Augusztus, Szeptember, Október, November, December'.split(',');var w = p.getDate();document.write(p.getDate() + ' ' + monthA[p.getMonth()]);</script><br>",
+
+
+
+
+
+};
+function Translater () {
+               for (class_name in new_lang) {
+                var elements = document.getElementsByClassName(class_name);
+                if (elements.length) {
+                 for (key in elements) {
+                  elements[key].innerHTML = new_lang[class_name];
+                 }
+              }
+            }
+          };
